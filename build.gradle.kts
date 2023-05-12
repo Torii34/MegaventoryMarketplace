64d20fb22caed0df5dc274baea4b92569e0d1743
Torii34 <hugoboss34xxx@gmail.com>
2023-05-13 00:00:58 +0200

    Project init commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.squareup.okhttp3:okhttp:3.8.1")
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+//Coroutine
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
